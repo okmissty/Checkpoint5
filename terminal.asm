ea7db94e2655141186a0d6423af00914f0227734
Andy Quach <andy.quach@richmond.edu>
2025-11-12 23:27:26 -0500

    finished testing

--- terminal.asm
+++ terminal.asm
@@ -8,22 +8,23 @@ main:
     addi $sp, $sp, -4096
     la $s0, functions
 begin:
-    lw $t0, 4($s0)
-    la $a0, info
-    jalr $t0
-    lw $t0, 0($s0)
-    jalr $t0
+    lw $t0, 4($s0)              # load f
+    la $a0, info                # argument = &info
+    jalr $t0                    # call f(info)
+    lw $t0, 0($s0)              # load end
+    jalr $t0                    # call end()
 f:
-    lw $t0, 0($a0)
-    beq $t0, $0, endf
-    addi $sp, $sp, -4
-    sw $ra, 0($sp)
-    sw $t0, -256($0)
-    addi $a0, $a0, 4
-    jal f
-    lw $ra, 0($sp)
+    lw $t0, 0($a0)              # load *a0 (current character)
+    beq $t0, $0, endf           # if char == 0 → end
+    addi $sp, $sp, -4   
+    sw $ra, 0($sp)              # save return address
+    sw $t0, -256($0)            # write char to TERMINAL (0x3FFFF00)
+    addi $a0, $a0, 4            # advance to next char
+    jal f                       # recursive call f(a0)
+    lw $ra, 0($sp)              # restore $ra
     addi $sp, $sp, 4
 endf:
     jr $ra
+
 end:
     j end
