9b6590c25b39b41cc5e4df6d87b2fe0f1ef42e99
Andy Quach <andy.quach@richmond.edu>
2025-11-13 23:36:30 -0500

    ctrl3

---
+++ Testcases/Hex-test/HexCtrl3.asm
@@ -0,0 +1,30 @@
+.text
+.globl main
+
+main:
+    # Initialize stack pointer
+    addi $sp,$0,-4096
+
+    # $t0 will hold the current digit (0..15)
+    addi $t0,$0,0         # digit = 0
+
+loop_digits:
+    # Write current digit directly to hex display
+    # 0x3FFFF30 == 0xFFFFFF30 == -208
+    sw   $t0,-208($0)     # HexController should show hex digit t0 (low 4 bits)
+
+    ########################################################
+    # Simple delay so you can see the digit change
+    ########################################################
+    addi $t2,$0,30000     # adjust as needed
+delay:
+    addi $t2,$t2,-1
+    bne  $t2,$0,delay
+
+    ########################################################
+    # Next digit: (t0 + 1) & 0xF  -> cycles 0..15
+    ########################################################
+    addi $t0,$t0,1        # t0++
+    andi $t0,$t0,0xF      # keep only low 4 bits
+
+    j    loop_digits
