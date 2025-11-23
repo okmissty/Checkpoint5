12b7bd7e54d4c8e7840306ef51af4f791b813012
Andy Quach <andy.quach@richmond.edu>
2025-11-22 22:34:13 -0500

    reassembled2

--- output/sys_joystick_test.asm
+++ output/sys_joystick_test.asm
@@ -5,24 +5,28 @@
 .text
 
 ###################################################
-# === MAIN PROGRAM FIRST ===
+# === MAIN PROGRAM ===
 ###################################################
 main:
 Loop:
     # Read joystick
-    jal  Joystick_ReadXY         # X→v0, Y→v1
+    jal  Joystick_ReadXY         # X -> v0, Y -> v1
+
+    # Save X,Y before we overwrite v0 with syscall codes
+    add  $t0, $v0, $zero         # t0 = X
+    add  $t1, $v1, $zero         # t1 = Y
 
     # Print "X="
-    li   $v0, 11
+    li   $v0, 11                 # print char
     li   $a0, 88                 # 'X'
     syscall
     li   $v0, 11
     li   $a0, 61                 # '='
     syscall
 
-    # Print X as integer
-    li   $v0, 1
-    add  $a0, $v0, $zero         # move $a0, $v0
+    # Print X as integer (from t0, not v0)
+    li   $v0, 1                  # print int
+    add  $a0, $t0, $zero         # a0 = X
     syscall
 
     # Print " Y="
@@ -35,9 +39,9 @@ Loop:
     li   $a0, 61                 # '='
     syscall
 
-    # Print Y as integer
+    # Print Y as integer (from t1)
     li   $v0, 1
-    add  $a0, $v1, $zero         # move $a0, $v1
+    add  $a0, $t1, $zero         # a0 = Y
     syscall
 
     # Newline
@@ -48,7 +52,7 @@ Loop:
     j Loop
 
 ###################################################
-# === JOYSTICK DRIVER ROUTINES AFTER MAIN ===
+# === JOYSTICK DRIVER ROUTINES ===
 ###################################################
 
 # Return X (0..15) in $v0
@@ -63,9 +67,7 @@ Joystick_ReadY:
     andi $v0, $v0, 0x000F
     jr   $ra
 
-# Return X and Y
-#   $v0 = X
-#   $v1 = Y
+# Return X and Y: v0 = X, v1 = Y
 Joystick_ReadXY:
     lw   $v0, -176($zero)        # X
     lw   $v1, -172($zero)        # Y
