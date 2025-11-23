e0f52a86ed8bf2637c74a839985d61b2cbad4968
Andy Quach <andy.quach@richmond.edu>
2025-11-22 22:27:44 -0500

    reassembled

--- CHECKPOINT5/sys_joystick_test.asm
+++ CHECKPOINT5/sys_joystick_test.asm
@@ -5,7 +5,50 @@
 .text
 
 ###################################################
-# === JOYSTICK DRIVER ROUTINES ===
+# === MAIN PROGRAM FIRST ===
+###################################################
+main:
+Loop:
+    # Read joystick
+    jal  Joystick_ReadXY         # X→v0, Y→v1
+
+    # Print "X="
+    li   $v0, 11
+    li   $a0, 88                 # 'X'
+    syscall
+    li   $v0, 11
+    li   $a0, 61                 # '='
+    syscall
+
+    # Print X as integer
+    li   $v0, 1
+    add  $a0, $v0, $zero         # move $a0, $v0
+    syscall
+
+    # Print " Y="
+    li   $v0, 11
+    li   $a0, 32                 # ' '
+    syscall
+    li   $v0, 11
+    li   $a0, 89                 # 'Y'
+    syscall
+    li   $a0, 61                 # '='
+    syscall
+
+    # Print Y as integer
+    li   $v0, 1
+    add  $a0, $v1, $zero         # move $a0, $v1
+    syscall
+
+    # Newline
+    li   $v0, 11
+    li   $a0, 10
+    syscall
+
+    j Loop
+
+###################################################
+# === JOYSTICK DRIVER ROUTINES AFTER MAIN ===
 ###################################################
 
 # Return X (0..15) in $v0
@@ -29,46 +72,3 @@ Joystick_ReadXY:
     andi $v0, $v0, 0x000F
     andi $v1, $v1, 0x000F
     jr   $ra
-
-###################################################
-# === MAIN ===
-###################################################
-main:
-Loop:
-    # Read joystick
-    jal  Joystick_ReadXY         # X→v0, Y→v1
-
-    # Print "X="
-    li   $v0, 11
-    li   $a0, 88                 # 'X'
-    syscall
-    li   $v0, 11
-    li   $a0, 61                 # '='
-    syscall
-
-    # Print X (integer)
-    li   $v0, 1
-    add  $a0, $v0, $zero         # move $a0, $v0
-    syscall
-
-    # Print " Y="
-    li   $v0, 11
-    li   $a0, 32                 # ' '
-    syscall
-    li   $v0, 11
-    li   $a0, 89                 # 'Y'
-    syscall
-    li   $a0, 61                 # '='
-    syscall
-
-    # Print Y (integer)
-    li   $v0, 1
-    add  $a0, $v1, $zero         # move $a0, $v1
-    syscall
-
-    # Newline
-    li   $v0, 11
-    li   $a0, 10
-    syscall
-
-    j Loop
